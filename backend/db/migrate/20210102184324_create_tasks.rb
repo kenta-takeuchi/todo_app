abd37c2155f04fb607908a3d8279cc349263e00d
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2021-01-03 03:46:36 +0900

    model, controllerの作成

---
+++ backend/db/migrate/20210102184324_create_tasks.rb
@@ -0,0 +1,15 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.string :status
+      t.belongs_to :category
+      t.integer :user_id
+      t.integer :responsible_id
+      t.timestamps
+    end
+    add_foreign_key :tasks, :users, column: :user_id
+    add_foreign_key :tasks, :users, column: :responsible_id
+  end
+end
